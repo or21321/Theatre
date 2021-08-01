c42bb86f65fe7fb76a780ba045c9a41da0d1daae
Or Hadar <ormoglob@gmail.com>
2021-08-01 13:12:21 +0300

    first

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/style/_theatre.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/theatre.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="theatre">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'theatre',
+  components: {
+  }
+}
+</script>
